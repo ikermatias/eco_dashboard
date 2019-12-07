e84b154995f28fa4f501b1ac82e4b70578388371
Sebastian garcia <josegar0218@gmail.com>
2019-12-07 15:18:34 -0500

    Update dash

--- main.py
+++ main.py
@@ -2,48 +2,44 @@ import pygame
 import RPi.GPIO as GPIO
 import subprocess
 
+count = 1;
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(3, GPIO.IN)
 GPIO.wait_for_edge(3, GPIO.FALLING)
 
-print("pulso")
+while count != 4:
+    pygame.init()
 
-pygame.init()
+    black = (0, 0, 0)
+    green = (0, 255, 0)
+    blue = (0, 0, 128)
 
-black = (0, 0, 0)
-green = (0, 255, 0)
-blue = (0, 0, 128)
+    # assigning values to X and Y variable
+    X = 400
+    Y = 400
 
-# assigning values to X and Y variable
-X = 400
-Y = 400
+    display_surface = pygame.display.set_mode((X, Y))
 
-display_surface = pygame.display.set_mode((X, Y))
+    # set the pygame window name
+    pygame.display.set_caption('Monitor ' + count)
 
-# set the pygame window name
-pygame.display.set_caption('Monitor 1')
+    font = pygame.font.Font('freesansbold.ttf', 32)
 
-font = pygame.font.Font('freesansbold.ttf', 32)
+    # create a text suface object,
+    # on which text is drawn on it.
+    text = font.render('Este es el monitor', True, green, blue)
 
-# create a text suface object,
-# on which text is drawn on it.
-text = font.render('Este es el monitor', True, green, blue)
+    textRect = text.get_rect()
 
-textRect = text.get_rect()
+    # set the center of the rectangular object.
+    # textRect.center = (X // 2, Y // 2)
 
-# set the center of the rectangular object.
-#textRect.center = (X // 2, Y // 2)
+    display_surface.fill(black)
 
-
-
-display_surface.fill(black)
-
-# copying the text surface object
-# to the display surface object
-# at the center coordinate.
-display_surface.blit(text, textRect)
-
-while True:
+    # copying the text surface object
+    # to the display surface object
+    # at the center coordinate.
+    display_surface.blit(text, textRect)
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -58,4 +54,8 @@ while True:
 
         pygame.display.update()
 
+    GPIO.wait_for_edge(3, GPIO.FALLING)
+    count = count + 1
 
+pygame.quit()
+quit()
